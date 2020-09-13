df728f002b819942e1c8dd7646811b3f80e6c56a
Maharsh Gheewala <maharshgheewala@Maharshs-MacBook-Pro.local>
2020-09-13 17:27:03 -0500

    Chat_body

--- whatsapp/src/Chat.js
+++ whatsapp/src/Chat.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import { Avatar, IconButton } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import AttachFile from "@material-ui/icons/AttachFile";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 import './Chat.css';
 
 function Chat() {
+
+    const [seed, setSeed] = useState("");
+
+    useEffect(() => {
+      setSeed(Math.floor(Math.random() * 5000));
+    }, []);
+
     return (
-        <div className="chat">
-            
+      <div className="chat">
+        <div className="chat_header">
+          <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+          <div className="chat_headerInfo">
+            <h3>RoomName</h3>
+            <p>Last Seen at ...</p>
+          </div>
+          <div className="chat_headerRight">
+            <IconButton>
+              <SearchOutlined />
+            </IconButton>
+            <IconButton>
+              <AttachFile />
+            </IconButton>
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          </div>
         </div>
-    )
+        <div className="chat_body"></div>
+        <div className="chat_footer"></div>
+      </div>
+    );
 }
 
 export default Chat;
