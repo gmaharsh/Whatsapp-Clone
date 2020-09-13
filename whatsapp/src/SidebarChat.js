9ac8b71f1474fa227510b1bd7187f446373d6905
Maharsh Gheewala <maharshgheewala@Maharshs-MacBook-Pro.local>
2020-09-13 16:56:32 -0500

    Sidebar_chats

--- whatsapp/src/SidebarChat.js
+++ whatsapp/src/SidebarChat.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import { Avatar } from '@material-ui/core';
+import React, {useEffect, useState} from 'react';
+
+import './SidebarChat.css';
 
 function SidebarChat() {
+
+    const [seed, setSeed] = useState("");
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000));
+    }, [])
+
     return (
-        <div className="SidebarChat">
-            <h1>Rachana</h1>
+      <div className="SidebarChat">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="sidebarChat_info">
+          <h2>Room Name</h2>
+          <p>Last Message...</p>
         </div>
-    )
+      </div>
+    );
 }
 
 export default SidebarChat;
