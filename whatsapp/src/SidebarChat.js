179e5d220e951bd2c51bc4547d9358a57066f218
Maharsh Gheewala <maharshgheewala@Maharshs-MacBook-Pro.local>
2020-09-13 16:49:06 -0500

    Sidebar

---
+++ whatsapp/src/SidebarChat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SidebarChat() {
+    return (
+        <div className="SidebarChat">
+            <h1>Rachana</h1>
+        </div>
+    )
+}
+
+export default SidebarChat;
